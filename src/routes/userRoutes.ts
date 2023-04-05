039262be55110e73d2415a755ebdb107e9b5fcc0
Prasoon Soni <prasoonsoni03@gmail.com>
2023-04-05 13:35:01 +0530

    Added router to login user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,5 +3,6 @@ import userController from "../controllers/userController"
 const router: Router = express.Router()
 
 router.post('/create', userController.createUser)
+router.post('/login', userController.loginUser)
 
 export default router
